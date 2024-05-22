cc4dfec80e03e6e8d8f78bc9441fd8e98c285c65
Killian Portier <killian.portier@yahoo.fr>
2024-05-22 13:51:08 +0200

    exo done

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,11 +5,30 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
   end
 
-  def show; end
+  def show
+    @review = Review.new
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
   def set_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,7 +1,9 @@
 class Restaurant < ApplicationRecord
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+
   has_many :reviews, dependent: :destroy
 
   validates :name, presence: true
   validates :address, presence: true
-  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :category, inclusion: { in: CATEGORY }
 end
